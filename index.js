8c01529f24f91fab776810f2e5cd5ca6e4f74182
mundi <christine.mundi@student.moringaschool.com$ git config --global user.email christine.mundi@student.moringaschool.com>
2023-03-26 12:28:54 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solution here
+const drivers = ['Kate', 'Josh', 'Tracy', 'NIky', 'Drew', 'Joy']
+
+const findMatching = (p,q) => p.filter(p => p.toLowerCase() === q.toLowerCase())
+
+const fuzzyMatch = (p,q) => p.filter(p => p.charAt(0) === q.charAt(0))
+
+const matchName = (p,q) => p.filter(p => p.name === q)
+
